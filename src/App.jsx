2902ea3e866df7d3efddefb3a9d3b111a45c4e0c
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-06-09 16:42:36 +0200

    git add . git commit -m "Add event counter feature"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
+const initialEvents =[
+  {id:1 , title:"Event1"},
+  {id:2, title:"Event2"},
+  {id:3 , title:"Event3"}
+];
+
 
 function App() {
+  const [events, setEvents]= useState(initialEvents);
+
+  const visibleEvents = events;
+  const count = visibleEvents.length;
+
   return (
-    <>
-    </>
-  )
+    <div>
+      <h2>Events found : {count}</h2>
+
+      <ul>
+        {visibleEvents.map(event=>(
+          <li key={event.id}> {event.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App
